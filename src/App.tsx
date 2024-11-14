f182093d757ec88e4d51b346b629c48acd8cbb5f
Henrique <henrique.v.curti@gmail.com>
2024-11-14 16:49:38 -0300

    add components

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
+import LoginPage from "./pages/login/LoginPage"
+
 function App() {
 
   return (
-    <div className="flex items-center justify-center min-h-screen bg-blue-100">
-      <h1 className="text-4xl font-bold text-blue-500">Hello, Tailwind with Vite and TypeScript!</h1>
-    </div>
+    <>
+      <LoginPage/>
+    </>    
   )
 }
 
